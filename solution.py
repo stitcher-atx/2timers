e262b3387541bc863659c1a4e3384d665552a0a4
Zach Hill <zach.ac.hill@gmail.com>
2019-02-07 16:44:08 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,24 +4,20 @@ class Timer:
        self.y = 0
 
 def solutioncheck():
-   if int(T1.x) + int(T2.x) == 15:
+   if int(T1.x) + int(T2.x) == solution:
        print("solved")
        exit()
-   elif int(T1.x) + int(T2.y) == 15:
+   elif int(T1.x) + int(T2.y) == solution:
        print("solved")
        exit()
-   elif int(T1.y) + int(T2.x) == 15:
+   elif int(T1.y) + int(T2.x) == solution:
        print("solved")
        exit()
-   elif int(T1.y) + int(T2.y) == 15:
+   elif int(T1.y) + int(T2.y) == solution:
        print("solved")
        exit()
    else:
        print("keep going")
-
-T1 = Timer(11)
-T2 = Timer(13)
-solution = 15
 
 def solver():
    if T1.x > T1.y:
@@ -29,12 +25,17 @@ def solver():
        T2.x = T2.x - T2.y
        T1.y = T1.x
        T1.x = 0
-   elif T1.x < T1.y:
+   else T1.x < T1.y:
        T1.x = T1.y - T2.y
        T1.y = T1.y - T1.x
        T2.x = T2.x + T2.y
        T2.y = 0
 
+T1 = Timer(11)
+T2 = Timer(13)
+solution = 15      
+      
+      
 while True:
    print("Timer 1 values: {0} & {1}".format(T1.x, T1.y))
    print("Timer 2 values: {0} & {1}".format(T2.x, T2.y))
